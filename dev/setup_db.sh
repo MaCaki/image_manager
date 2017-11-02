4aaf3d37b679c529db9ffa051803498758a4c988
Matthew C Kim <m.cadier.kim@gmail.com>
2017-11-02 13:57:53 -0700

    db init script improved

--- dev/setup_db.sh
+++ dev/setup_db.sh
@@ -5,11 +5,11 @@ echo "If this is the first time you are running this dev db, docker will attempt
 PGPASSWORD='dev_db_password'
 
 # if the container is not created yet, build it.
-if [ ! "$(docker ps -a -f name=image_manger_dev_db)" ]; then
+if [ ! "$(docker ps -a -f name=image_manger_dev_db | grep image_manger_dev_db)" ]; then
     echo 'Creating new postgres container...'
     docker run --name image_manger_dev_db -p 5432:5432 -e POSTGRES_PASSWORD=$PGPASSWORD -d postgres
 else
-    echo 'Restarting the postgres conainer...'
+    echo 'Container is already created, restarting the postgres conainer...'
     docker start image_manger_dev_db
 fi
 
@@ -18,6 +18,14 @@ while [ ! "$(docker ps -q -f name=image_manger_dev_db)" ]; do
     echo 'sleeping'
     sleep 1
 done
+
+# A hacky way to wait for the new container instance to finish starting the postgres service so that 
+# the db init commands below don't fail.
+while [ ! "$(PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c 'select 1234' | grep 1234)" ]; do
+    echo 'Waiting for database to recieve connections...'
+    sleep 1
+done
+
 
 declare -a cmnds=(
     "CREATE ROLE image_manager WITH LOGIN PASSWORD 'image_manager';"
